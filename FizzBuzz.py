c1e4f66cfa98d3b0dcf2ec6b49cb29b15d3e346a
MaliDo <75333966+MaliDo@users.noreply.github.com>
2022-03-17 00:37:21 +0300

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,10 @@
+l1 = range(1, 1001)
+for n in l1:
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
