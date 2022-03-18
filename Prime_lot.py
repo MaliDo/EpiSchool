12f91701786547f7f7b548f7a27eeb75889000e5
MaliDo <75333966+MaliDo@users.noreply.github.com>
2022-03-18 22:38:11 +0300

    Update Prime_lot.py

--- Prime_lot.py
+++ Prime_lot.py
@@ -1,25 +1,18 @@
 def is_prime(x):
-    if x > 1 and x % 2 != 0:
-        return True
-    elif x == 2:
-        return True
+    if x > 1:
+        for i in range(2, x):
+            if x % i == 0:
+                return False
+        else:
+            return True
     else:
-        return False
+        return False 
     
 def primes_below(n):
     l1 = []
-    l2 = []
     first = 0
     while first < n:
-        l1.append(first)
+        if is_prime(first) == True:
+            l1.append(first)
         first = first + 1
-    for x in l1:
-        if is_prime(x) == True:
-            l2.append(x)
-    print(l2)
-
-primes_below(24)
-
-
-
-
+    print(l1)
